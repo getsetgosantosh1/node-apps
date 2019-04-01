f9f8ce0071ffb0855c4c5183d1a723f37e106801
getsetgosantosh1 <santoshkumar@Santoshs-MacBook-Air.local>
2019-04-01 23:21:39 +0530

    sasas

--- exp-ejs-1/routes/index.js
+++ exp-ejs-1/routes/index.js
@@ -3,9 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Express123'
-  });
+  res.render('pages/index');
+});
+
+router.get('mat', function (req, res, next) {
+  res.render('pages/index-mat');
+});
+
+router.get('boot', function (req, res, next) {
+  res.render('pages/index-boot');
 });
 
 module.exports = router;
